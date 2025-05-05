f283808a34a2feeca3000f104e8a0f201a546f80
andrehsvictor <andrehsvictor@gmail.com>
2025-05-05 10:24:34 -0300

    feat: implement UserDetails and UserDetailsService for authentication

---
+++ src/main/java/andrehsvictor/camly/common/security/UserDetailsImpl.java
@@ -0,0 +1,43 @@
+package andrehsvictor.camly.common.security;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import andrehsvictor.camly.user.model.User;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private static final long serialVersionUID = -2761636667441154736L;
+    
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority(role))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEmailVerified();
+    }
+
+}
