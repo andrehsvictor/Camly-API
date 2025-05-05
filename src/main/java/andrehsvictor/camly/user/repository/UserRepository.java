44448bb9b94017be024104f19e80512c155470e5
andrehsvictor <andrehsvictor@gmail.com>
2025-05-05 10:24:25 -0300

    feat: add query method to find User by username or email

--- src/main/java/andrehsvictor/camly/user/repository/UserRepository.java
+++ src/main/java/andrehsvictor/camly/user/repository/UserRepository.java
@@ -1,11 +1,16 @@
 package andrehsvictor.camly.user.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import andrehsvictor.camly.user.model.User;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+    @Query("SELECT u FROM User u WHERE u.username = :usernameOrEmail OR u.email = :usernameOrEmail")
+    Optional<User> findByUsernameOrEmail(String usernameOrEmail);
 
 }
