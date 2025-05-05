78ec51b988620ce29f0e3930a7df7c3559fae3b9
andrehsvictor <andrehsvictor@gmail.com>
2025-05-05 15:59:13 -0300

    feat: enhance JWT validation by adding RevokedJwtValidator for token revocation checks

--- src/main/java/andrehsvictor/camly/common/config/JwtConfig.java
+++ src/main/java/andrehsvictor/camly/common/config/JwtConfig.java
@@ -2,12 +2,16 @@ package andrehsvictor.camly.common.config;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtValidators;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.validation.annotation.Validated;
@@ -20,9 +24,11 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
 
 @Validated
 @Configuration
+@RequiredArgsConstructor
 public class JwtConfig {
 
     @NotNull(message = "JWT private key must not be null")
@@ -33,9 +39,13 @@ public class JwtConfig {
     @Value("${camly.jwt.public-key.path}")
     private RSAPublicKey publicKey;
 
+    private final List<OAuth2TokenValidator<Jwt>> jwtValidators;
+
     @Bean
     JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withPublicKey(publicKey).build();
+        jwtDecoder.setJwtValidator(JwtValidators.createDefaultWithValidators(jwtValidators));
+        return jwtDecoder;
     }
 
     @Bean
