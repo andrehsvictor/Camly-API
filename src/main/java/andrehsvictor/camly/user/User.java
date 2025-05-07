75fc85d8801f8e105c7e3d9de3cf7dcdc93d000a
andrehsvictor <andrehsvictor@gmail.com>
2025-05-07 19:47:42 -0300

    refactor: replace roles collection with single role column in User entity

--- src/main/java/andrehsvictor/camly/user/User.java
+++ src/main/java/andrehsvictor/camly/user/User.java
@@ -2,20 +2,15 @@ package andrehsvictor.camly.user;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
-import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -103,11 +98,10 @@ public class User implements Serializable {
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt = LocalDateTime.now();
 
-    @ElementCollection
-    @CollectionTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id"))
-    @Column(name = "name")
     @Builder.Default
-    private Set<String> roles = new HashSet<>();
+    @Column(name = "role", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
 
     @PreUpdate
     void preUpdate() {
