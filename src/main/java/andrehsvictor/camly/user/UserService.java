d1588b1f5f7defde18e69ffa844405e6b723a7a1
andrehsvictor <andrehsvictor@gmail.com>
2025-05-08 10:10:10 -0300

    feat: enhance UserService with caching for improved performance and add deleteById method

--- src/main/java/andrehsvictor/camly/user/UserService.java
+++ src/main/java/andrehsvictor/camly/user/UserService.java
@@ -2,6 +2,10 @@ package andrehsvictor.camly.user;
 
 import java.util.UUID;
 
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -12,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@CacheConfig(cacheNames = "users")
 public class UserService {
 
     private final UserRepository userRepository;
@@ -21,6 +26,7 @@ public class UserService {
         return userMapper.userToUserDto(user);
     }
 
+    @Cacheable(key = "'filters_' + #query + '_' + #username + '_' + #pageable.pageNumber + '_' + #pageable.pageSize")
     public Page<User> getAllWithFilters(
             String query,
             String username,
@@ -28,46 +34,65 @@ public class UserService {
         return userRepository.findAllWithFilters(query, username, pageable);
     }
 
+    @Cacheable(key = "'userById_' + #id")
     public User getById(UUID id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "ID", id));
     }
 
+    @Cacheable(key = "'userByEmail_' + #email")
     public User getByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "email", email));
     }
 
+    @Cacheable(key = "'userByProviderId_' + #providerId")
     public User getByProviderId(String providerId) {
         return userRepository.findByProviderId(providerId)
                 .orElseThrow(() -> new ResourceNotFoundException(
                         "User", "provider ID", providerId));
     }
 
+    @Cacheable(key = "'userByEmailVerificationToken_' + #token")
     public User getByEmailVerificationToken(String token) {
         return userRepository.findByEmailVerificationToken(token)
                 .orElseThrow(() -> new ResourceNotFoundException(
                         "Action token not found"));
     }
 
+    @Cacheable(key = "'userByResetPasswordToken_' + #token")
     public User getByResetPasswordToken(String token) {
         return userRepository.findByResetPasswordToken(token)
                 .orElseThrow(() -> new ResourceNotFoundException(
                         "Action token not found"));
     }
 
+    @Caching(evict = {
+            @CacheEvict(key = "'userById_' + #user.id", condition = "#user.id != null"),
+            @CacheEvict(key = "'userByEmail_' + #user.email", condition = "#user.email != null"),
+            @CacheEvict(key = "'userByProviderId_' + #user.providerId", condition = "#user.providerId != null"),
+            @CacheEvict(key = "'existsByUsername_' + #user.username", condition = "#user.username != null"),
+            @CacheEvict(key = "'existsByEmail_' + #user.email", condition = "#user.email != null")
+    })
     public User save(User user) {
         return userRepository.save(user);
     }
 
+    @Cacheable(key = "'existsByUsername_' + #username")
     public boolean existsByUsername(String username) {
         return userRepository.existsByUsername(username);
     }
 
+    @Cacheable(key = "'existsByEmail_' + #email")
     public boolean existsByEmail(String email) {
         return userRepository.existsByEmail(email);
     }
 
+    @Caching(evict = {
+            @CacheEvict(key = "'userById_' + #id"),
+            @CacheEvict(key = "'filters_*'", allEntries = true),
+            @CacheEvict(cacheNames = "accounts", allEntries = true)
+    })
     public void deleteById(UUID id) {
         userRepository.deleteById(id);
     }
