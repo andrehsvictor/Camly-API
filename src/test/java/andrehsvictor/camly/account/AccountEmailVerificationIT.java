01a3391e00d511c0697754ea9e512d3b22592160
andrehsvictor <andrehsvictor@gmail.com>
2025-05-09 10:29:54 -0300

    test: enhance email verification integration test to validate email content in MailHog

--- src/test/java/andrehsvictor/camly/account/AccountEmailVerificationIT.java
+++ src/test/java/andrehsvictor/camly/account/AccountEmailVerificationIT.java
@@ -1,6 +1,9 @@
 package andrehsvictor.camly.account;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDateTime;
@@ -19,6 +22,12 @@ class AccountEmailVerificationIT extends AbstractAccountIntegrationTest {
     @Test
     @DisplayName("Should send verification email")
     void shouldSendVerificationEmail() {
+        User user = userService.getById(testUser.getId());
+        user.setEmailVerified(false);
+        user.setEmailVerificationToken(null);
+        user.setEmailVerificationTokenExpiresAt(null);
+        userService.save(user);
+
         SendActionEmailDto emailDto = SendActionEmailDto.builder()
                 .email(testUser.getEmail())
                 .action(EmailSendingAction.VERIFY_EMAIL)
@@ -37,6 +46,17 @@ class AccountEmailVerificationIT extends AbstractAccountIntegrationTest {
         assertTrue(updatedUser.getEmailVerificationToken() != null);
         assertTrue(updatedUser.getEmailVerificationTokenExpiresAt() != null);
         assertTrue(updatedUser.getEmailVerificationTokenExpiresAt().isAfter(LocalDateTime.now()));
+
+        when().get(String.format("%s/api/v1/messages", getMailHogUrl()))
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("size()", equalTo(1))
+                .body("[0].To[0].Mailbox", equalTo(testUser.getEmail().split("@")[0]))
+                .body("[0].To[0].Domain", equalTo(testUser.getEmail().split("@")[1]))
+                .body("[0].Content.Headers.Subject[0]", equalTo("Verify your email"))
+                .body("[0].Content.Headers.From[0]", equalTo("noreply@camly.io"))
+                .body("[0].Content.Body", containsString("token="))
+                .body("[0].Content.Body", containsString("Verify My Email"));
     }
 
     @Test
