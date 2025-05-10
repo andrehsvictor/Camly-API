57ce85d65710f7cfe6e284e84d3693a66aeae155
andrehsvictor <andrehsvictor@gmail.com>
2025-05-10 14:28:26 -0300

    feat: add LoggingAspect for enhanced logging of method execution and error handling

---
+++ src/main/java/andrehsvictor/camly/aspect/LoggingAspect.java
@@ -0,0 +1,84 @@
+package andrehsvictor.camly.aspect;
+
+import java.util.Arrays;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Pointcut("within(@org.springframework.stereotype.Repository *)" +
+            " || within(@org.springframework.stereotype.Service *)" +
+            " || within(@org.springframework.web.bind.annotation.RestController *)")
+    public void springBeanPointcut() {
+    }
+
+    @Pointcut("within(andrehsvictor.camly..*)")
+    public void applicationPackagePointcut() {
+    }
+
+    private Logger logger(JoinPoint joinPoint) {
+        return LoggerFactory.getLogger(joinPoint.getSignature().getDeclaringTypeName());
+    }
+
+    @AfterThrowing(pointcut = "applicationPackagePointcut() && springBeanPointcut()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        Logger log = logger(joinPoint);
+        log.error("Exception in {}.{}() with cause = '{}' and exception = '{}'",
+                joinPoint.getSignature().getDeclaringTypeName(),
+                joinPoint.getSignature().getName(),
+                e.getCause() != null ? e.getCause() : "NULL",
+                e.getMessage());
+    }
+
+    @Around("applicationPackagePointcut() && springBeanPointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        Logger log = logger(joinPoint);
+
+        if (log.isDebugEnabled()) {
+            log.debug("Enter: {}.{}() with argument[s] = {}",
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(),
+                    Arrays.toString(joinPoint.getArgs()));
+        }
+
+        try {
+            long start = System.currentTimeMillis();
+            Object result = joinPoint.proceed();
+            long executionTime = System.currentTimeMillis() - start;
+
+            if (log.isDebugEnabled()) {
+                log.debug("Exit: {}.{}() with result = {}, execution time = {} ms",
+                        joinPoint.getSignature().getDeclaringTypeName(),
+                        joinPoint.getSignature().getName(),
+                        result,
+                        executionTime);
+            }
+
+            if (executionTime > 500) {
+                log.warn("Slow operation detected: {}.{}() took {} ms",
+                        joinPoint.getSignature().getDeclaringTypeName(),
+                        joinPoint.getSignature().getName(),
+                        executionTime);
+            }
+
+            return result;
+
+        } catch (IllegalArgumentException e) {
+            log.error("Illegal argument: {} in {}.{}()",
+                    Arrays.toString(joinPoint.getArgs()),
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName());
+            throw e;
+        }
+    }
+}
