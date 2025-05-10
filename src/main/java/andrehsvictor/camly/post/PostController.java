c28ade1e3b5954de7ed2ff6fdf4348cb119a591d
andrehsvictor <andrehsvictor@gmail.com>
2025-05-10 08:22:24 -0300

    feat: add endpoints for retrieving posts and user-specific posts with filtering options

--- src/main/java/andrehsvictor/camly/post/PostController.java
+++ src/main/java/andrehsvictor/camly/post/PostController.java
@@ -2,12 +2,16 @@ package andrehsvictor.camly.post;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import andrehsvictor.camly.post.dto.CreatePostDto;
@@ -46,6 +50,38 @@ public class PostController {
         Post post = postService.update(id, updatePostDto);
         PostDto postDto = postService.toDto(post);
         return ResponseEntity.ok(postDto);
+    }
+
+    @GetMapping("/api/v1/posts")
+    public ResponseEntity<Page<PostDto>> getAll(
+            @RequestParam(required = false, name = "q") String query,
+            @RequestParam(required = false) String username,
+            Pageable pageable) {
+        Page<Post> posts = postService.getAllWithFilters(query, username, pageable);
+        Page<PostDto> postDtos = posts.map(postService::toDto);
+        return ResponseEntity.ok(postDtos);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public ResponseEntity<PostDto> getById(@PathVariable UUID id) {
+        Post post = postService.getById(id);
+        PostDto postDto = postService.toDto(post);
+        return ResponseEntity.ok(postDto);
+    }
+
+    @GetMapping("/api/v1/posts/stats")
+    public ResponseEntity<PostStats> getPostStats() {
+        PostStats postStats = postService.getPostStatsByCurrentUser();
+        return ResponseEntity.ok(postStats);
+    }
+
+    @GetMapping("/api/v1/user/{userId}/posts")
+    public ResponseEntity<Page<PostDto>> getAllByUserId(
+            @PathVariable UUID userId,
+            Pageable pageable) {
+        Page<Post> posts = postService.getAllByUserId(userId, pageable);
+        Page<PostDto> postDtos = posts.map(postService::toDto);
+        return ResponseEntity.ok(postDtos);
     }
 
 }
